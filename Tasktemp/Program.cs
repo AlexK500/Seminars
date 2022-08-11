8f21500f70b2377a240afd78032c11841a1ab7b0
AlexKudrya <Alex@ndr.ru>
2022-08-12 01:40:02 +0300

    Домашка 19 - первый способ через цифры и массив

--- Tasktemp/Program.cs
+++ Tasktemp/Program.cs
@@ -1,7 +1,3 @@
-﻿Console.Write("Ваше имя: ");
-// Ввод строки
-string name = Console.ReadLine();
-// Приветствие + подсказка
-Console.Write("Привет, " + name + "\nВведите Ваш рост в см: ");
-// Ввод числа. Чтобы сразу получить число, используйте методы класса Convert
-double height = Convert.ToDouble(Console.ReadLine())/100;
+﻿Console.WriteLine("Введите число: ");
+int chetver = Convert.ToInt32(Console.ReadLine());
+
