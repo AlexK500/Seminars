2b9f5d498d4be8ac346ca47960f20734805a5e26
AlexKudrya <Alex@ndr.ru>
2022-08-17 00:42:36 +0300

    Задача №29 - задачу решил, но как сам понял, условие не совсем понятно

---
+++ Hometask029/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+// из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите количество элементов массива:\t");
+int elementsCount = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[elementsCount];
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент массива под индехом {i}:\t");
+    myArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("[");
+for (int i = 0; i < myArray.Length - 1; i++)
+    Console.Write($" {myArray[i]}, ");
+Console.Write($" {myArray[myArray.Length - 1]}]");
