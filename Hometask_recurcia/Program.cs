080bac65b8b65b9f11a82e69386816c45c320e0a
AlexKudrya <Alex@ndr.ru>
2022-08-24 13:09:51 +0300

    Задача на вычесление факториала методом рекурсии

---
+++ Hometask_recurcia/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Factorial(int n)
+{
+    if (n == 1) return 1;
+ 
+    return n * Factorial(n - 1);
+}
+Console.Write(n+ " -> "+Factorial(n));
